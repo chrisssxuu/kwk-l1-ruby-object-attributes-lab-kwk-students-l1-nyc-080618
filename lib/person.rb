320ea2b8957205f5d12df4dfeafb1bd72e25e192
chrisssxuu <chris01.xu@gmail.com>
2018-08-09 19:13:24 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,13 @@ class Person
   def name 
     @name
   end
-  def name = (name)
+  def name=(name)
     @name = name
   end
   def job 
     @job
   end
-  def job = (job) 
+  def job=(job) 
     @job = job
   end
 end
