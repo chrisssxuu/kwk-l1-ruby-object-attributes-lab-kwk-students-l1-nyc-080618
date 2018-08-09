79df993b01140bdf50998d1b8de89dd63431c7d8
chrisssxuu <chris01.xu@gmail.com>
2018-08-09 19:05:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # dog.rb
+class Dog 
+  def name = (new_name)
+    @name = new_name
+  end
+  def name 
+    @name
+  end
+  def breed = (breed_name) 
+    @breed = breed_name
+  end
+  def breed 
+    @breed 
+  end
+end
